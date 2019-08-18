2b151e11326b090d8e80a79cdf8bcf2661eaa7e9
Rakhshanda Mujib <47360491+RakhshandaMujib@users.noreply.github.com>
2019-08-18 23:51:30 +0530

    Delete 03 Sum of Digits.py

--- 03 Sum of Digits.py
+++
@@ -1,9 +0,0 @@
-T = int(input())
-sums = [0 for i in range(T)]
-for i in range(T):
-    num = int(input())
-    while num > 0:
-    	num, sums[i] = (num // 10), (sums[i] + (num % 10))
-for i in sums:
-	print(i)    
-    
