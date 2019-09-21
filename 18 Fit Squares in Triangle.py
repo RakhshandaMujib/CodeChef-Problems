c30c9df47799b855feca2cd3ee38ddc33e8e2918
Rakhshanda Mujib <47360491+RakhshandaMujib@users.noreply.github.com>
2019-09-21 11:56:54 +0530

    Update 18 Fit Squares in Triangle.py

--- 18 Fit Squares in Triangle.py
+++ 18 Fit Squares in Triangle.py
@@ -1,9 +1,9 @@
 for _ in range(int(input())):
 	B = int(input()) #Base length
 	'''
-	If B is even, then the maximum number of squares (n) there can be in one row 
+	  If B is even, then the maximum number of squares (n) there can be in one row 
 	(|| to the base) is (B - 2) / 2. 
-	If B is odd, then n will be (B - 3) / 2.
+	  If B is odd, then n will be (B - 3) / 2.
 	The total number of squares can be found out by summing up the first n 
 	natural numbers using the progression formula (n * (n + 1)) / 2.
 	'''
